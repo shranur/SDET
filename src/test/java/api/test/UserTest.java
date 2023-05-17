add3dfae4fc66b681d8ad40a37025cd8094f0aac
Anurag <anuragshrivastava007@gmail.com>
2023-05-17 14:19:59 +0530

    Initial commit

---
+++ src/test/java/api/test/UserTest.java
@@ -0,0 +1,56 @@
+package api.test;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+import com.github.javafaker.Faker;
+
+import api.endpoints.UserEndPoints;
+import api.payload.User;
+import io.restassured.response.Response;
+public class UserTest {
+	
+				Faker faker;
+				User userPayload;
+	
+				@BeforeClass
+				
+				public void setupData()
+				{
+					
+					faker = new Faker();
+					userPayload = new User();
+					
+					userPayload.setId(faker.idNumber().hashCode());
+					userPayload.setFirstname(faker.name().firstName());
+					userPayload.setLastname(faker.name().lastName());
+					userPayload.setEmail(faker.internet().emailAddress());
+					userPayload.setPassword(faker.internet().password(5, 10));
+					userPayload.setPhone(faker.phoneNumber().cellPhone());
+					
+				}
+				
+				
+				@Test(priority =1)
+				public void testPostUser()
+				{
+					Response response =UserEndPoints.createUser(userPayload);
+					response.then().log().all();
+					
+					Assert.assertEquals(response.getStatusCode(), 200);
+					
+				}
+				
+				@Test(priority =2)
+				public void getTestUserByName()
+				{
+					 Response response=UserEndPoints.readUser(this.userPayload.getUsername());
+					 response.then().log().all();
+					 Assert.assertEquals(response.getStatusCode(), 200);
+					
+				}
+				
+
+}
